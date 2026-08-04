58324ce2723ca9682e81a37c9d9751caf806721f
Facundo Giglio <facundogiglioing@gmail.com>
2026-08-04 09:20:12 -0300

    feat: mejorar la interacción del componente EntityListItem al convertirlo en un botón y agregar manejo de clics

--- src/components/EntityListItem.tsx
+++ src/components/EntityListItem.tsx
@@ -1,13 +1,12 @@
-import type { ReactNode } from "react";
+import type { MouseEventHandler, ReactNode } from "react";
 import { cn } from "@/lib/cn";
-import { DeleteButton } from "./base/DeleteButton";
-import { EditButton } from "./base/EditButton";
 
 type EntityListItemProps = {
   title: string;
   subtitle: string;
   icon: ReactNode;
   value?: ReactNode;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
   isActive?: boolean;
   editHref?: string;
   onDelete?: () => Promise<void>;
@@ -24,23 +23,28 @@ export function EntityListItem({
   icon,
   value,
   isActive = false,
+  onClick,
   className,
   subtitleClassName,
   valueClassName,
   iconClassName,
   actions,
 }: EntityListItemProps) {
+
+
   return (
-    <div
+    <button
+      type="button"
+      onClick={onClick}
       className={cn(
-        "group flex items-center justify-between rounded-xl border p-4 transition-all cursor-pointer",
+        "group flex items-center justify-between rounded-xl border p-4 transition-all cursor-pointer w-full",
         isActive
           ? "border-blue-500 ring-2 ring-blue-500/10 shadow-lg"
           : "border-slate-200 hover:shadow-md",
         className,
       )}
     >
-      <div className="flex items-center gap-4">
+      <div className="flex justify-center gap-4">
         <div
           className={cn(
             "rounded-lg p-2",
@@ -52,7 +56,7 @@ export function EntityListItem({
         >
           {icon}
         </div>
-        <div>
+        <div className="flex flex-col items-start justify-center min-w-0">
           <h3 className="font-semibold text-slate-900">{title}</h3>
           <p
             className={cn(
@@ -79,6 +83,6 @@ export function EntityListItem({
 
         {actions}
       </div>
-    </div>
+    </button>
   );
 }
